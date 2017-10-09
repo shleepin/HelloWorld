1517d6d89474a791f21aa326d4bbeb44152623d4
Justice Coleman <justicetcoleman@gmail.com>
2017-10-08 22:19:30 -0400

    Initial commit

---
+++ app/src/main/java/com/example/shleepin/helloworld/MyActivity.java
@@ -0,0 +1,13 @@
+package com.example.shleepin.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.Hello_World);
+    }
+}
